c2401a38f8354ac84f18fe8cd743a323533e428d
Neon <andrewrules903@gmail.com>
2022-10-26 18:06:05 +0000

    test for scroll

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry);
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
+const boxes = document.querySelectorAll('.box')
 
-        }
-    });
-});
+window.addEventListener('scroll', checkBoxes)
+    checkBoxes()
 
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
+    function checkBoxes(){
+        const triggerBottom = window.innerHeight /5 * 4 
+        boxes.forEach(box => {
+            const boxTop = box.getBoundingClientRect().top
+            if(boxTop < triggerBottom){
+                box.classList.add('show')
+            } else{
+                box.classList.remove('show')
+            }
+        })
+    }
